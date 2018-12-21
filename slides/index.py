103b768faec9626bfe3b78fbe431a280a3b6d09a
Marco Mazzucco <marco.mazzucco@pl.ibm.com>
2018-12-21 12:49:31 +0100

    Removed some unnessary files for now

--- slides/index.py
+++ slides/index.py
@@ -105,58 +105,58 @@ import datetime
 import jinja2
 import yaml
 
-items = yaml.load(open("index.yaml"))
+# items = yaml.load(open("index.yaml"))
 
-# Items with a date correspond to scheduled sessions.
-# Items without a date correspond to self-paced content.
-# The date should be specified as a string (e.g. 2018-11-26).
-# It can also be a list of two elements (e.g. [2018-11-26, 2018-11-28]).
-# The latter indicates an event spanning multiple dates.
-# The first date will be used in the generated page, but the event
-# will be considered "current" (and therefore, shown in the list of
-# upcoming events) until the second date.
+# # Items with a date correspond to scheduled sessions.
+# # Items without a date correspond to self-paced content.
+# # The date should be specified as a string (e.g. 2018-11-26).
+# # It can also be a list of two elements (e.g. [2018-11-26, 2018-11-28]).
+# # The latter indicates an event spanning multiple dates.
+# # The first date will be used in the generated page, but the event
+# # will be considered "current" (and therefore, shown in the list of
+# # upcoming events) until the second date.
 
-for item in items:
-    if "date" in item:
-        date = item["date"]
-        if type(date) == list:
-            date_begin, date_end = date
-        else:
-            date_begin, date_end = date, date
-        suffix = {
-                1: "st", 2: "nd", 3: "rd",
-                21: "st", 22: "nd", 23: "rd",
-                31: "st"}.get(date_begin.day, "th")
-        # %e is a non-standard extension (it displays the day, but without a
-        # leading zero). If strftime fails with ValueError, try to fall back
-        # on %d (which displays the day but with a leading zero when needed).
-        try:
-            item["prettydate"] = date_begin.strftime("%B %e{}, %Y").format(suffix)
-        except ValueError:
-            item["prettydate"] = date_begin.strftime("%B %d{}, %Y").format(suffix)
-        item["begin"] = date_begin
-        item["end"] = date_end
+# for item in items:
+#     if "date" in item:
+#         date = item["date"]
+#         if type(date) == list:
+#             date_begin, date_end = date
+#         else:
+#             date_begin, date_end = date, date
+#         suffix = {
+#                 1: "st", 2: "nd", 3: "rd",
+#                 21: "st", 22: "nd", 23: "rd",
+#                 31: "st"}.get(date_begin.day, "th")
+#         # %e is a non-standard extension (it displays the day, but without a
+#         # leading zero). If strftime fails with ValueError, try to fall back
+#         # on %d (which displays the day but with a leading zero when needed).
+#         try:
+#             item["prettydate"] = date_begin.strftime("%B %e{}, %Y").format(suffix)
+#         except ValueError:
+#             item["prettydate"] = date_begin.strftime("%B %d{}, %Y").format(suffix)
+#         item["begin"] = date_begin
+#         item["end"] = date_end
 
-today = datetime.date.today()
-coming_soon = [i for i in items if i.get("date") and i["end"] >= today]
-coming_soon.sort(key=lambda i: i["begin"])
-past_workshops = [i for i in items if i.get("date") and i["end"] < today]
-past_workshops.sort(key=lambda i: i["date"], reverse=True)
-self_paced = [i for i in items if not i.get("date")]
-recorded_workshops = [i for i in items if i.get("video")]
+# today = datetime.date.today()
+# coming_soon = [i for i in items if i.get("date") and i["end"] >= today]
+# coming_soon.sort(key=lambda i: i["begin"])
+# past_workshops = [i for i in items if i.get("date") and i["end"] < today]
+# past_workshops.sort(key=lambda i: i["date"], reverse=True)
+# self_paced = [i for i in items if not i.get("date")]
+# recorded_workshops = [i for i in items if i.get("video")]
 
-template = jinja2.Template(TEMPLATE)
-with open("index.html", "w") as f:
-    f.write(template.render(
-    	title="Container Training",
-    	coming_soon=coming_soon,
-    	past_workshops=past_workshops,
-    	self_paced=self_paced,
-    	recorded_workshops=recorded_workshops
-    	).encode("utf-8"))
+# template = jinja2.Template(TEMPLATE)
+# with open("index.html", "w") as f:
+#     f.write(template.render(
+#     	title="Container Training",
+#     	coming_soon=coming_soon,
+#     	past_workshops=past_workshops,
+#     	self_paced=self_paced,
+#     	recorded_workshops=recorded_workshops
+#     	).encode("utf-8"))
 
-with open("past.html", "w") as f:
-	f.write(template.render(
-		title="Container Training",
-		all_past_workshops=past_workshops
-		).encode("utf-8"))
+# with open("past.html", "w") as f:
+# 	f.write(template.render(
+# 		title="Container Training",
+# 		all_past_workshops=past_workshops
+# 		).encode("utf-8"))
--- slides/k8s/kubenet.md
+++ slides/k8s/kubenet.md
@@ -63,9 +63,7 @@
 
 ## Kubernetes network model: in practice
 
-- The nodes that we are using have been set up to use [Weave](https://github.com/weaveworks/weave)
-
-- We don't endorse Weave in a particular way, it just Works For Us
+- The nodes that we are using have been set up to use [calico](https://www.projectcalico.org)
 
 - Don't worry about the warning about `kube-proxy` performance
 
